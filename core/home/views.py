e8490ae7070fd0ee70f1f13bf5a6bc69060deea2
Harrish P T <harrishpt@gmail.com>
2023-08-08 23:17:44 +0530

    GET and POST method

--- core/home/views.py
+++ core/home/views.py
@@ -1,13 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET','POST','PUT'])
+@api_view(['GET','POST'])
 def index(request):
     courses={
-        'course_name':'Python',
-        'learn':['flask','django'],
-        'course_provider':'Coursera',
-    }
-    return Response(courses)
+            'course_name':'Python',
+            'learn':['flask','django'],
+            'course_provider':'Coursera',
+        }
+    if request.method=="GET":
+        print('You Hit a GET Method')
+        return Response(courses)
+    elif request.method=="POST":
+        print('You Hit a POST Method')
+        return Response(courses)
+
 
 # Create your views here.
